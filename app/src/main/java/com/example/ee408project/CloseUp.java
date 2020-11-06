4bffa1da8bcf3e233ebb53c6540036890229b790
robbie-decker <59452453+robbie-decker@users.noreply.github.com>
2020-11-06 17:06:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ee408project/CloseUp.java
@@ -0,0 +1,13 @@
+package com.example.ee408project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CloseUp extends AppCompatActivity {
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.closeup);
+        }
+}
