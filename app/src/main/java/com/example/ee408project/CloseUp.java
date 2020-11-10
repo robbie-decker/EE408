9f11bddaac7322655df1cc83510c4bee1dad4ff8
robbie-decker <59452453+robbie-decker@users.noreply.github.com>
2020-11-10 18:08:48 -0500

    Making some progress specifically to the shopping cart

--- app/src/main/java/com/example/ee408project/CloseUp.java
+++ app/src/main/java/com/example/ee408project/CloseUp.java
@@ -1,13 +1,61 @@
 package com.example.ee408project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
 public class CloseUp extends AppCompatActivity {
 
+        private StoreDatabase dbHelper;
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.closeup);
+
+            final Bundle bundle = getIntent().getExtras();
+
+            TextView name = (TextView)findViewById(R.id.name);
+            //TextView description = (TextView)findViewById(R.id.description);
+            TextView price = (TextView)findViewById(R.id.price);
+
+            assert name != null;
+            name.setText(bundle.getString("name"));
+            //assert description != null;
+            //description.setText(bundle.getString("description"));
+            BigDecimal priceVal = BigDecimal.valueOf(bundle.getInt("price"),2); // we had stored price as a whole integer to include cents e.g 1.00 was stored as 100
+            assert price != null;
+            price.setText("Price: $"+priceVal);
+
+            dbHelper = new StoreDatabase(this);
+            try {
+                dbHelper.open();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            Button button = (Button)findViewById(R.id.buy);
+            assert button != null;
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (dbHelper.addToCart(bundle.getInt("_id"), "1")){
+                        Intent intent = new Intent(CloseUp.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                        Toast.makeText(CloseUp.this, "Successfully added to shopping cart", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(CloseUp.this, "Oops! Something went wrong. Please try again.", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
         }
 }
--- app/src/main/java/com/example/ee408project/MainActivity.java
+++ app/src/main/java/com/example/ee408project/MainActivity.java
@@ -6,12 +6,23 @@ package com.example.ee408project;
         import android.os.Bundle;
         import android.view.View;
 
+        import java.sql.SQLException;
+
 public class MainActivity extends AppCompatActivity {
+    private StoreDatabase dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new StoreDatabase(this);
+        try {
+            dbHelper.open();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void onClickListener(View view){
